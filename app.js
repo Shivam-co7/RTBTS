95ddf2edaf11f37a5fac98e1b0a9d744b4122796
Shivam Chaturvedi <shivamchaturvedi268@gmail.com>
2023-05-02 16:04:37 +0530

    main changes

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const ejs           = require('ejs');
 const expressLayout = require('express-ejs-layouts');
 const bodyParser    = require('body-parser');
 const sql           = require('mysql');
+const sessions      = require('express-session');
+const cookieParser  = require('cookie-parser');
 const app = express();
 
 //Setting up port, View engine, custom delimiter (i.e. changing '%' to '?' here!) & using the layout engine
@@ -13,6 +15,19 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 ejs.delimiter = '?';
+
+//saving session & cookie details 
+let oneDay = 24 * 60 * 60 * 1000;
+
+app.use(cookieParser());
+app.use(sessions({
+    secret: "SomeSecretKey1212",
+    saveUninitialized:true,
+    cookie: { maxAge: oneDay },
+    resave: false 
+}));
+
+
 
 // creating connection with database
 const conn = sql.createConnection({
@@ -42,10 +57,31 @@ app.post('/login', (req, res) => {
         if(err){console.log("err!"); throw err;}
 
         if(result.length > 0){
-            res.redirect('/?workin_logged_in!');
+            
+            res.redirect('/dashboard');
         }
         else{
             res.redirect('/?failed_attempt!');
+        }
+    });
+});
+
+app.post('/register', (req, res) => {
+    const name     = req.body.name;
+    const email    = req.body.email;
+    const password = req.body.password;
+    let sql        = "INSERT INTO users (name, email, password) VALUES (?)";
+    let values     = [[name, email, password]];
+    // console.log("name= " + name + "email= " + email + " password= " + password);      //for debugging
+
+    conn.query(sql, values, (err, result) => {
+        if(err){console.log("err!"); throw err;}
+
+        if(result){
+            res.redirect('/?Registered_Successfully!');
+        }
+        else{
+            res.redirect('/?failed_attempt_try_again!');
         }
     });
 });
